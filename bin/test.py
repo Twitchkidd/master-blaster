7452e9b05a32f6e697ed21c86578546b24006ab0
Gareth Field <field.gareth@gmail.com>
2020-07-05 11:48:19 -0400

    save

--- bin/test.py
+++ bin/test.py
@@ -4,11 +4,17 @@ import subprocess
 
 
 def main():
+    dicticle = {}
     result = subprocess.run(["git", "branch"], universal_newlines=True,
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(type(result.stdout))
-    print(result.stderr)
-    print(result.returncode)
+    print(result.stdout)
+    # print(type(result.stdout))
+    # print(result.stderr)
+    # print(result.returncode)
+    dicticle['yay'] = 'dev' in result.stdout
+    print(dicticle["yay"])
+    if None:
+        print(dicticle.get('nay'))
 
 
 if __name__ == "__main__":
